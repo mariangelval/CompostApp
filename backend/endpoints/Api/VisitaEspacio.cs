b5a0aba6b1bdc2f2bcab1d3d8be8a66c6ceda27c
Mariangel <mariangel.valerioet12d1@gmail.com>
2024-10-25 16:53:27 -0300

    Endpoints

--- backend/endpoints/Api/VisitaEspacio.cs
+++ backend/endpoints/Api/VisitaEspacio.cs
@@ -4,5 +4,6 @@ public class VisitaEspacio
 
     public short idVisitaEspacio { get; set; }
     public DateTime fecha { get; set; }
+    public int kilos { get; set; }
     public string descripcion { get; set;}
 }
